59b6ea943e52dfed7ef8f9c9a2261b8c73d26554
Henri Kerola <henri.kerola@vaadin.com>
2015-03-19 10:26:59 -0700

    Vaadin version updated to 7.4.2.

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,7 +3,7 @@ import sbt.Keys._
 
 object Dependencies {
 
-  val vaadinVersion = "7.3.6"
+  val vaadinVersion = "7.4.2"
 
   val vaadinServer = "com.vaadin" % "vaadin-server" % vaadinVersion
   val vaadinClient = "com.vaadin" % "vaadin-client" % vaadinVersion
